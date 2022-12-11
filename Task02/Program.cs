b9611bcd9109760753720251a958df8a9297b047
Inna7777 <peshkova-inna@mail.ru>
2022-12-11 18:15:13 +0400

    Add the second task

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int number)
+{
+    
+    int sum = 0;
+       while (number > 0)
+    {
+      sum = sum + number%10;
+      number = number / 10;
+    }
+    return sum;
+}
+int sum = SumNum(number);
+Console.WriteLine($"Сумма цифр = {sum}");
